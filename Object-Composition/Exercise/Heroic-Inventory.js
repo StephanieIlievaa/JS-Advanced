60ca842566cda06421c24591b2ddda9b2f3802c3
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-05-17 23:30:15 +0300

    update

--- Object-Composition/Exercise/Heroic-Inventory.js
+++ Object-Composition/Exercise/Heroic-Inventory.js
@@ -1,18 +1,18 @@
 function registerHeroes(input) {
     let result = [];
-while(input.length) {
-    let hero = input.shift();
-    let [name, level, itemsString] = heroe.split(' / ');
-let level = number(level);
-const items = itemsString.split(', ');
+    while (input.length) {
+        let hero = input.shift();
+        let [name, level, itemsString] = heroe.split(' / ');
+        let level = number(level);
+        const items = itemsString ? itemsString.split(', ') : [];
 
-result.push({
-        name,
-        level, 
-        items,
-    });
-}
-return JSON.stringify(result);
+        result.push({
+            name,
+            level,
+            items: items ? items : [],
+        });
+    }
+    return JSON.stringify(result);
 
 }
 registerHeroes(['Isacc / 25 / Apple, GravityGun', 'Derek / 12 / BarrelVest, DestructionSword', 'Hes / 1 / Desolator, Sentinel, Antara']);
