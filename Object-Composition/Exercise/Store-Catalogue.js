9c1da69bdfbab1f50dfdfb7010f3f502e877a17c
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-05-20 22:25:16 +0300

    update

--- Object-Composition/Exercise/Store-Catalogue.js
+++ Object-Composition/Exercise/Store-Catalogue.js
@@ -1,4 +1,4 @@
 function solve(input) {
-
+console.log(input);
 }
-solve(['Appricot : 20.4', 'Fridge : 1500', 'TV : 1499', 'Deodorant : 10', 'Boiler : 300', 'Apple : 1.25','Anti-Bug Spray:15', 'T-shirt:10');
+solve(['Appricot : 20.4', 'Fridge : 1500', 'TV : 1499', 'Deodorant : 10', 'Boiler : 300', 'Apple : 1.25','Anti-Bug Spray:15', 'T-shirt:10']);
