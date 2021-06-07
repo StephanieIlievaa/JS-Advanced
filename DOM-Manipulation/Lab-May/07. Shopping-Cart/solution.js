635180e22a9d6ef0d576f7fa34a6d41a20fde93a
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-06-07 18:50:55 +0300

    update

--- DOM-Manipulation/Lab-May/07. Shopping-Cart/solution.js
+++ DOM-Manipulation/Lab-May/07. Shopping-Cart/solution.js
@@ -3,7 +3,8 @@ function solve() {
    let textAreaElement = document.querySelector('textarea');
    let checkoutButtonElement = document.querySelector('.checkout');
    let sum = 0;
-   let uniqueProducts = [];
+   let products = [];
+
    for (const addProductButton of addProductButtons) {
       addProductButton.addEventListener('click', (e) => {
          e.currentProductElement = e.currentTarget.parentElement.parentElement;
@@ -12,18 +13,18 @@ function solve() {
          let productPrice = currentProductElement.querySelectorAll('.product-line-price');
 
          sum += productPrice;
-         uniqueProducts.push(productName);
-         textAreaElement.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the card.\n `;
+         products.push(productName);
+         textAreaElement.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the card.\n` ;
 
       });
    }
    checkoutButtonElement.addEventListener('click', (e) => {
       let uniqueProducts = products.reduce((a, x) =>{
          if (!a.includes(x)) {
-            a.push(x)
+            a.push(x);
          }
          return a;
-      } , []);
+      }, []);
       textAreaElement.textContent += `You bought ${uniqueProducts.join(', ')} for ${sum.toFixed(2)}.`;
    });
 }
