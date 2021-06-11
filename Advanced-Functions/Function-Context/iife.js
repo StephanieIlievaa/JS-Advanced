4941a59c4f59423625103d5f22e7f30ae4584336
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-06-11 16:08:39 +0300

    update

---
+++ Advanced-Functions/Function-Context/iife.js
@@ -0,0 +1,11 @@
+let result = (function () {
+
+    let name = "Peter";
+    
+    return name;
+    
+    })();
+    
+    // Immediately creates the output:
+    
+    console.log(result); // Peter
