ab677472d1d2664498a553f7c386653d87a636c4
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-06-26 19:48:46 +0300

    Update cookingByNumbers.js

--- Syntax-Functions-Statements/Exercise/cookingByNumbers.js
+++ Syntax-Functions-Statements/Exercise/cookingByNumbers.js
@@ -33,4 +33,21 @@ function cookingByNumbers(input) {
     }
 }
 
+
+
+
+//function cookingByNumbers(...arr) {
+ // let num = +arr.shift()
+
+//  while (arr.length > 0) {
+  //  let currentOperation = arr.shift()
+   // if (currentOperation == 'chop') console.log(num /= 2)
+   // if (currentOperation == 'dice') console.log(num = Math.sqrt(num))
+   // if (currentOperation == 'spice') console.log(num += 1)
+  //  if (currentOperation == 'bake') console.log(num *= 3)
+  //  if (currentOperation == 'fillet') console.log(num *= 0.80)
+//  }
+//}
+
+
 cookingByNumbers([9, 'dice', 'spice', 'chop', 'bake', 'fillet']);
