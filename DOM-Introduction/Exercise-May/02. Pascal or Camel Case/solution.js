783178b228b33c893678e2a793c6cd0c86aef201
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-06-26 20:33:57 +0300

    Update solution.js

--- DOM-Introduction/Exercise-May/02. Pascal or Camel Case/solution.js
+++ DOM-Introduction/Exercise-May/02. Pascal or Camel Case/solution.js
@@ -1,4 +1,22 @@
 function solve() {
-let input = document.getElementById('text').value;
-let currentCase = document.getElementById('naming-convention').value;
+
+    let text = document.getElementById('input').value.split('.').map(e => e.trim()).filter(e => e !== '');
+    const n = 3
+    const result = new Array(Math.ceil(text.length / n))
+        .fill()
+        .map(_ => text.splice(0, n))
+
+    const output = document.getElementById('output');
+    result.forEach(e => {
+        let text = e.reduce((acc, e) => acc +`${e}.`, '');
+        createAppendParagraph(document.createTextNode(text))
+    });
+
+
+    function createAppendParagraph(text) {
+        let p = document.createElement('p');
+        p.appendChild(text)
+        output.appendChild(p)
+    }
+
 }
