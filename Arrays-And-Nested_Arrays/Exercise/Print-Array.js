26ba4ffcc02d9e6a15bd81bd72c82c387b5efa82
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-06-26 20:00:16 +0300

    Update Print-Array.js

--- Arrays-And-Nested_Arrays/Exercise/Print-Array.js
+++ Arrays-And-Nested_Arrays/Exercise/Print-Array.js
@@ -2,8 +2,11 @@
 // function expressions in JS are not hoisted, like function declarations
 // we can`t use function expressions before we create them\
 //to use ternary operator we need to have expressions(evaluated to produce a value), not statements(executed to make something happen)
- const solve = (array, delimiter) =>  {
-     return array.join(delimiter);
+ //const solve = (array, delimiter) =>  {
+    // return array.join(delimiter);
+function solve(array, delimeter) {
+    return array.join(delimeter);
+}
 
 
 // let result =  '';
@@ -12,6 +15,6 @@
 // }
 // return result;
 
-}
+//}
 //console.log(solve(['One','Two','Three','Four','Five'],'-'));
 console.log(solve(['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_'));
